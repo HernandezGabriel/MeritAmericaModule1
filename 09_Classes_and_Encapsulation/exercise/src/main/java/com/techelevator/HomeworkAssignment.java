d276bee5059a5a4dea0ab19b6a6996466ad64baa
Gabriel Hernandez <hernandezgabriel533@gmail.com>
2022-03-15 20:00:15 -0500

    hWClassDone

--- 09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
+++ 09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -2,4 +2,39 @@ package com.techelevator;
 
 public class HomeworkAssignment {
 
+
+    private String submitterName;
+
+    private int possibleMarks;
+    private int earnedMarks;
+
+    public HomeworkAssignment(int possibleMarks, String submitterName){
+        this.possibleMarks=possibleMarks;
+        this.submitterName=submitterName;
+    }
+
+    public void setEarnedMarks(int earnedMarks){
+        this.earnedMarks=earnedMarks;
+    }
+
+    public String getLetterGrade() {
+        double grade = ((double)getEarnedMarks()/(double)getPossibleMarks());
+        if(grade>=.9){ return "A";}
+        if(grade>=.8&&grade<.89){ return "B";}
+        if(grade>=.7&&grade<.79){ return "C";}
+        if(grade>=.6&&grade<.69){ return "D";}
+        else{ return "F";}
+    }
+
+    public String getSubmitterName() {
+        return submitterName;
+    }
+
+    public int getPossibleMarks() {
+        return possibleMarks;
+    }
+
+    public int getEarnedMarks() {
+        return earnedMarks;
+    }
 }
---
+++ techsquad/src/main/java/Excersize.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Excersize {
+
+    public static void main(String[] args ){
+        Scanner sc = new Scanner(System.in);
+        String s;
+        String index;
+        int n=0;
+        System.out.println("Please enter a string: ");
+        s=sc.next();
+
+        while(s.indexOf('a', n)>-1){
+
+
+        }
+
+
+
+    }
+
+
+}
