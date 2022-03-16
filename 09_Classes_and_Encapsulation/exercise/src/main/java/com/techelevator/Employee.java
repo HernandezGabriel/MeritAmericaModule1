cfb5245d012b95ffeac78f2b4977b9ef4600e3e4
Gabriel Hernandez <hernandezgabriel533@gmail.com>
2022-03-15 20:34:23 -0500

    Step 2&3 Done

--- 09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Employee.java
+++ 09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Employee.java
@@ -2,4 +2,67 @@ package com.techelevator;
 
 public class Employee {
 
+    private int employeeId;
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private String department;
+    private double annualSalary;
+
+    //raiseSalary()
+
+    public void raiseSalary(double percent){
+        annualSalary=annualSalary+ annualSalary*(percent/100);
+
+    }
+
+
+
+    //Constructor
+
+    public Employee(int employeeId, String firstName,
+                    String lastName, double salary){
+        this.employeeId=employeeId;
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.annualSalary=salary;
+
+    }
+
+    //Getters
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return lastName+", "+firstName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getAnnualSalary() {
+        return annualSalary;
+    }
+
+    //Setters
+
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
--- 09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/FruitTree.java
+++ 09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/FruitTree.java
@@ -2,4 +2,33 @@ package com.techelevator;
 
 public class FruitTree {
 
+    private String typeOfFruit;
+    private int piecesOfFruit;
+
+    //Constructor
+    public FruitTree(String typeOfFruit, int startingPiecesOfFruit) {
+        this.typeOfFruit=typeOfFruit;
+        this.piecesOfFruit=startingPiecesOfFruit;
+    }
+
+    //Getters
+
+    public String getTypeOfFruit() {
+        return typeOfFruit;
+    }
+
+    public int getPiecesOfFruitLeft() {
+        return piecesOfFruit;
+    }
+
+    //pickFruit()
+
+    public boolean pickFruit(int numberOfPiecesToRemove){
+        if (piecesOfFruit>=numberOfPiecesToRemove){
+            piecesOfFruit= piecesOfFruit-numberOfPiecesToRemove;
+            return true;
+        }
+        else return false;
+    }
+
 }
